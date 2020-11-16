0da4e32573e4afcfa46550a9f2e1c53672aaba9d
EvilRojo <rojo3ds@gmail.com>
2020-11-16 17:49:42 -0600

    0.5

--- js/Batalla.js
+++ js/Batalla.js
@@ -1,6 +1,7 @@
 export class Batalla {
     /** @param {string} posicion */
     verPos(posicion) {
+        document.getElementById("salida1").src="../img/tumba.jpg"
         return "img/tumba.jpg";
     }
   }
