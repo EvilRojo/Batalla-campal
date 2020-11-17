04f72885156263cc39a5617cf0d3f76f294a841e
EvilRojo <rojo3ds@gmail.com>
2020-11-16 19:55:28 -0600

    0.5

--- js/Batalla.js
+++ js/Batalla.js
@@ -2,8 +2,9 @@ export class Batalla {
     /** @param {string} posicion */
     verPos(posicion) {
         switch(posicion){
+            
             case "1,1":
-                document.getElementById("salida"+posicion).src="img/tumba.jpg"
+                document.getElementById(`salida${posicion}`).src="img/tumba.jpg"
             break;
             case "1,2":
                 document.getElementById("salida1.2").src="img/tumba.jpg"
