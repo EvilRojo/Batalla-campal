0c2726df4f236e1f9c48fd9c3db7b72fe838ace4
EvilRojo <rojo3ds@gmail.com>
2020-11-17 14:28:33 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -2,7 +2,7 @@ export class Batalla {
     /** @param {string} posicion */
     verPos(posicion) {
         document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
-        const soldados = ["1,1", "1,3", "1,5"];
+        
         soldados.forEach(explosion);
         function explosion(item) {
             if(posicion===item){
--- js/inicio.js
+++ js/inicio.js
@@ -1,7 +1,22 @@
 export class nuevosol {
     /** @param {string} modo */
+    /** @param {Array} soldados*/
+    
 
     selecmo(modo){
-        location.href ="juego.html";
+        for(i = 0; i < 25; i++){
+            var x=Math.random() * (11 - 1) + 1;
+            var y=Math.random() * (11 - 1) + 1;
+            var f=x+","+y;
+            soldados.push(f);
+
+        }
+        soldados.forEach(explosion);
+        function explosion(item) {
+            if(posicion===item){
+                console.log(item);
+            }
+        }
+        //location.href ="juego.html";
     }
   }
