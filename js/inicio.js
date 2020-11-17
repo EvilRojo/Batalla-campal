1e9152979f9d85c16e04caa4b32c2f5ebb1cc9df
EvilRojo <rojo3ds@gmail.com>
2020-11-17 14:41:33 -0600

    0.8

--- js/inicio.js
+++ js/inicio.js
@@ -5,8 +5,8 @@ export class nuevosol {
     selecmo(modo){
         const soldados = [];
         for(var i = 0; i < 25; i++){
-            var x=Math.random() * (11 - 1) + 1;
-            var y=Math.random() * (11 - 1) + 1;
+            var x=Math.floor(Math.random() * (11 - 1) + 1);
+            var y=Math.floor(Math.random() * (11 - 1) + 1);
             var f=x+","+y;
             soldados.push(f);
 
