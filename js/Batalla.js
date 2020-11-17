037040a46f14d930166496c828b62da90d8ec931
EvilRojo <rojo3ds@gmail.com>
2020-11-17 01:34:09 -0600

    0.7

--- js/Batalla.js
+++ js/Batalla.js
@@ -10,4 +10,14 @@ export class Batalla {
             }
         }
     }
+    verPos2(posicion) {
+        document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
+        const soldados = ["1,1", "1,3", "1,5"];
+        soldados.forEach(explosion);
+        function explosion(item) {
+            if(posicion===item){
+                document.getElementById(`salida${posicion}`).src="img/tumba.jpg"
+            }
+        }
+    }
   }
