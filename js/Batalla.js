b9216e451f932e8e7b0cbf5a558eb61dab7b3dce
EvilRojo <rojo3ds@gmail.com>
2020-11-16 21:15:22 -0600

    0.6

--- js/Batalla.js
+++ js/Batalla.js
@@ -1,10 +1,15 @@
 export class Batalla {
     /** @param {string} posicion */
     verPos(posicion) {
-        if(posicion==="1,1"){
-            document.getElementById(`salida${posicion}`).src="img/tumba.jpg"
-        }else{
-            document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
+        const soldados = ["1,1", "2,3", "1,5"];
+        soldados.forEach(explosion);
+        
+        function explosion(item, index) {
+            if(posicion===item){
+                document.getElementById(`salida${posicion}`).src="img/tumba.jpg"
+            }else{
+                document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
+            }
         }
     }
   }
