c8b3ebeb4a162b4bf5570c890f508727344d83d8
EvilRojo <rojo3ds@gmail.com>
2020-11-18 00:48:34 -0600

    0.8

--- js/inicio.js
+++ js/inicio.js
@@ -1,7 +1,12 @@
 export class nuevosol {
     /** @param {string} modo */
     selecmo(modo){
+      if(modo=="2"){
         location.href ="juego.html";
+      }else{
+        location.href ="juego2.html";
+      }
+        
     }
 
 }
