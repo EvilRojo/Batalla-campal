40931339e1bc9eb5b32de0c26592d956277292bb
EvilRojo <rojo3ds@gmail.com>
2020-11-18 00:06:07 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -2,7 +2,7 @@ export class Batalla {
     /** @param {string} posicion */
         
         verPos(posicion) {
-            var x=A;
+            var x="A";
             document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
             const soldados=["1,1", "3,7", "1,6","9,9","9,2","3,6","5,9","8,6","4,8","2,3","1,8","6,9","3,1","4,4","8,2","7,4","6,4","4,6","5,7","2,4"];
             soldados.forEach(explosion);
@@ -14,7 +14,7 @@ export class Batalla {
             }
         }
         verPos2(posicion) {
-            var x=B;
+            var x="B";
             document.getElementById(`alida${posicion}`).src="img/pasto.jpg"
             const soldados=["1,5", "3,4", "5,6","8,7","9,3","7,7","5,8","7,1","9,4","9,8","5,8","2,2","4,3","7,5","6,3","3,5","1,9","8,2","6,1","5,3"];
             soldados.forEach(explosion);
@@ -27,10 +27,10 @@ export class Batalla {
         }
 
         bloqueo(x){
-            if(x==A){
-                x=B;
+            if(x=="A"){
+                x="B";
             }else{
-                x=A;
+                x="A";
             }
         }
 
