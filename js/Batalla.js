c576167523f285c42c48bd0095733d9006e7e2a6
EvilRojo <rojo3ds@gmail.com>
2020-11-17 23:42:55 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -3,12 +3,9 @@ export class Batalla {
     
         verPos(posicion) {
             document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
-            var x=2;
-            const soldados=[]
-            if(x != 1){
-                soldados=this.soldados();
-                x=1
-            }
+            var x=Number;
+            const soldados=[];
+            soldados=this.arr();
             soldados.forEach(explosion);
             function explosion(item) {
                 if(posicion===item){
@@ -26,16 +23,16 @@ export class Batalla {
                 }
             }
         }
-        soldados() {
-            const soldados=[];
+        arr(arreglo) {
+            arreglo=[];
             for (var i = 0; i < 25; i++) {
               var x = Math.floor(Math.random() * (11 - 1) + 1);
               var y = Math.floor(Math.random() * (11 - 1) + 1);
               var f = x + "," + y;
-              soldados.push(f);
+              arreglo.push(f);
           
             }
-            return soldados;
+            return arreglo;
     
           }
     }
