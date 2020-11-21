df844b7903d62b04bf2a9146b46b33b440e4ef08
EvilRojo <rojo3ds@gmail.com>
2020-11-21 03:44:37 -0600

    1.1

--- js/vsPC.js
+++ js/vsPC.js
@@ -108,7 +108,7 @@ export class Batalla {
     }
         arr(arreglo) {
             arreglo=[];
-            for (var i = 0; i < 45; i++) {
+            for (var i = 0; i < 30; i++) {
               var x = Math.floor(Math.random() * (11 - 1) + 1);
               var y = Math.floor(Math.random() * (11 - 1) + 1);
               var f = x + "," + y;
