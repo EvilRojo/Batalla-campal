0b3cd959b582e8a115e15b3b5c1eeb16ce18c7a1
EvilRojo <rojo3ds@gmail.com>
2020-11-20 22:05:51 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -3,6 +3,9 @@ export class Batalla {
     /** @param {number} numero */
         
         verPos(posicion, numero) {
+            if(numero==null){
+                numero=0;
+            }
             var x="A";
             document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
             const soldados=this.arr();
