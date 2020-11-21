853ad7e4997fc2067eaf3f60a3f1e653eed8ceb3
EvilRojo <rojo3ds@gmail.com>
2020-11-21 01:10:50 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -49,39 +49,39 @@ export class Batalla {
                             x=x;
                             y=y+i;
                         break;
-                        case i<=19:
+                        case i>9 && i<=19:
                             x=2;
                             y=y+(i-10);
                         break;
-                        case i<=29:
+                        case i>19 && i<=29:
                             x=3;
                             y=y+(i-20);
                         break;
-                        case i<=39:
+                        case i>29 && i<=39:
                             x=4;
                             y=y+(i-30);
                         break;
-                        case i<=49:
+                        case i>39 && i<=49:
                             x=5;
                             y=y+(i-40);
                         break;
-                        case i<=59:
+                        case i>49 && i<=59:
                             x=6;
                             y=y+(i-50);
                         break;
-                        case i<=69:
+                        case i>59 && i<=69:
                             x=7;
                             y=y+(i-60);
                         break;
-                        case i<=79:
+                        case i>69 && i<=79:
                             x=8;
                             y=y+(i-70);
                         break;
-                        case i<=89:
+                        case i>79 && i<=89:
                             x=9;
                             y=y+(i-80);
                         break;
-                        case i<=99:
+                        case i>89 && i<=99:
                             x=10;
                             y=y+(i-90);
                         break;
