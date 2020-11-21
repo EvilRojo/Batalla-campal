103e230052af227007c9b41bab97a4c597b04ac2
EvilRojo <rojo3ds@gmail.com>
2020-11-21 02:14:02 -0600

    1.0

--- js/vsPC.js
+++ js/vsPC.js
@@ -35,6 +35,7 @@ export class Batalla {
                 x="C";
                 this.bloqueo(x);
             }
+            console.log(numero+numero2);
             return numero, numero2;
         }
 
