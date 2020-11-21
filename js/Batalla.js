1ac5dab5d4431139773e726f30bfa099005d7abc
EvilRojo <rojo3ds@gmail.com>
2020-11-21 00:35:30 -0600

    0.8

--- js/Batalla.js
+++ js/Batalla.js
@@ -5,7 +5,7 @@ export class Batalla {
         verPos(posicion, numero) {
             var x="A";
             document.getElementById(`salida${posicion}`).src="img/pasto.jpg"
-            document.getElementById(`salida${posicion}`).disabled=true;
+
             const soldados=this.arr();
             soldados.forEach(explosion);
             this.bloqueo(x);
@@ -23,7 +23,7 @@ export class Batalla {
         verPos2(posicion, numero) {
             var x="B";
             document.getElementById(`alida${posicion}`).src="img/pasto.jpg"
-            document.getElementById(`salida${posicion}`).disabled=true;
+            document.getElementById(`alida${posicion}`).disabled=true;
             const soldados=this.arr();
             soldados.forEach(explosion);
             this.bloqueo(x);
@@ -41,8 +41,55 @@ export class Batalla {
 
         bloqueo(x){
             if(x=="A"){
-                document.getElementById(`w1`).disabled=true;
-                x="B";
+                for(var i = 0; i < 100; i++){
+                    var x=1;
+                    var y=1;
+                    switch (i){
+                        case (i<=9):
+                            x=x;
+                            y=y+i;
+                        break;
+                        case (i<=19):
+                            x=2;
+                            y=y+(i-10);
+                        break;
+                        case (i<=29):
+                            x=3;
+                            y=y+(i-20);
+                        break;
+                        case (i<=39):
+                            x=4;
+                            y=y+(i-30);
+                        break;
+                        case (i<=49):
+                            x=5;
+                            y=y+(i-40);
+                        break;
+                        case (i<=59):
+                            x=6;
+                            y=y+(i-50);
+                        break;
+                        case (i<=69):
+                            x=7;
+                            y=y+(i-60);
+                        break;
+                        case (i<=79):
+                            x=8;
+                            y=y+(i-70);
+                        break;
+                        case (i<=89):
+                            x=9;
+                            y=y+(i-80);
+                        break;
+                        case (i<=99):
+                            x=10;
+                            y=y+(i-90);
+                        break;
+                    }
+                    document.getElementById(`salida${x},${y}`).disabled=true;
+                }
+                
+                
             }else{
                 x="A";
             }
